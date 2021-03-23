68d1c0b9c9b7c9e409bd1cfc714db6105c997b78
Mus-Kah <58074573+Mus-Kah@users.noreply.github.com>
2021-03-23 18:44:32 +0100

    Automation of the preprocessing for clustering

--- run/clustering_main.py
+++ run/clustering_main.py
@@ -7,5 +7,6 @@ if __name__ == '__main__':
     spark = spark()
     data = read_dataset(file=file, spark=spark, delimiter=",")
     cols_for_clustering = cols_for_clustering(data)
-    for col in cols_for_clustering:
-        print(kmeans_list(column_to_list(data, col)))
+    if cols_for_clustering:
+        for col in cols_for_clustering:
+            print(kmeans_list(column_to_list(data, col)))
